70813cdd71de950e86b00b8d90c9ece74b7c6f1b
Quang Trinh <khacquang.trinh@gmail.com>
2020-08-20 23:24:06 +0700

    Fix header overflow

--- smart-watch-landing-page/src/components/Header/Action.vue
+++ smart-watch-landing-page/src/components/Header/Action.vue
@@ -1,10 +1,10 @@
 <template>
-  <ul class="w-1/3 hidden md:inline-flex justify-end flex-gap-6">
+  <ul class="w-1/3 hidden md:inline-flex items-center justify-end flex-gap-6">
     <li v-for="(item, index) in items" :key="index">
       <span
         class="link has-transition"
         :class="{
-          'link--has-border border-2 border-white rounded-full px-12 py-3  hover:bg-white hover:text-theme-primary':
+          'link--has-border border-2 border-white block rounded-full px-10 py-2 hover:bg-white hover:text-theme-primary':
             item.outline
         }"
         @click.prevent="showModal"
